020b63dc56c5c94e8378599302a1f4e5e705b02f
root <root@DESKTOP-8UCTSB1>
2023-02-19 07:18:47 -0800

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
+ * main - A C program that prints using puts function
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-
-
+/**
+ * main - A C program that prints using Printf function
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
