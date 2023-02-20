0c77f14ac0b9b0b0a8feda5160cbf0b21f61ab2e
root <root@DESKTOP-8UCTSB1>
2023-02-19 16:10:57 -0800

    Done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints a new line to the standard error
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	gets("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
