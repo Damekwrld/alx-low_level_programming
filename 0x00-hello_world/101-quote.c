8f578555987dcfaa7e6f8f20a2cb6c764bb6052e
Damekwrld <danielmayowa49@gmail.com>
2023-02-20 15:46:30 -0800

    Done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - A C program that prints a new line to the standard error
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
